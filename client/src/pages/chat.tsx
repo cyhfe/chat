031a66af0b74a5440e554c477c6e91df0ab1339c
chenyuhao <cccchenyuhao@gmail.com>
2022-01-19 12:07:34 +0800

    update

---
+++ client/src/pages/chat.tsx
@@ -0,0 +1,12 @@
+import LeftPanel from "../components/leftPanel"
+import RightPanel from "../components/rightPanel"
+import Main from "../components/main"
+export default function Chat() {
+  return (
+    <div>
+      <LeftPanel />
+      <Main />
+      <RightPanel />
+    </div>
+  )
+}
